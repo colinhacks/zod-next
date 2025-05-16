d60aa359ca7b8bdc734e370831157cb80a5eafcb
Colin McDonnell <colinmcd94@gmail.com>
2025-05-15 21:52:06 -0700

    Object parse

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
-import {string} from "zod/v4";
+import * as z from "zod/v4";
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
 
-  console.log(string().parse("John Doe"));;
+  console.log(z.object({name: z.string()}).parse({name: 'hello world'}))
   // return NextResponse.redirect(new URL('/home', request.url))
 
   // do nothing
